0b4a5e6725a748bf1485d846392c788e6ee10e85
byeongik11 <48646936+byeongik11@users.noreply.github.com>
2024-04-15 12:01:48 +0900

    Delete Chapter16_comb directory

--- Chapter16_comb/src/member/action/Action.java
+++
@@ -1,8 +0,0 @@
-package member.action;
-
-import javax.servlet.http.*;
-import vo.ActionForward;
-
-public interface Action {
-	public ActionForward execute(HttpServletRequest request,HttpServletResponse response) throws Exception;
-}
